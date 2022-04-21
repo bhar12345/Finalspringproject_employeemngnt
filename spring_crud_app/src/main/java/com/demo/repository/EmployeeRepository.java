ba19be838991ed3ad4947cb58cd35659f47bcb89
bhar12345 <say2bhargavi@gmail.com>
2022-04-21 13:30:14 +0530

    Finalproject

---
+++ spring_crud_app/src/main/java/com/demo/repository/EmployeeRepository.java
@@ -0,0 +1,12 @@
+package com.demo.repository;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.demo.model.Employee;
+
+public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
+    Employee getByName(String name);
+
+    Employee findByName(String name);
+}
